083ef8ff853215f913cb5a3cd283895c6a83e6bc
Víctor López <217356204+victor-lopz@users.noreply.github.com>
2026-03-01 12:05:57 +0100

    add edo_duffing with no noise at all to make it faster

--- src/ode.py
+++ src/ode.py
@@ -1,8 +1,7 @@
 import numpy as np
-import scipy
 from typing import Tuple
 
-def edo_duffing(t, z, random, epsilon, funcio_soroll):
+def edo_duffing_soroll(t, z, random, epsilon, funcio_soroll):
     """Paràmetres:
         t: temps
         z: posició al pla R^2
@@ -14,3 +13,12 @@ def edo_duffing(t, z, random, epsilon, funcio_soroll):
     x, y = z
     soroll = epsilon *  random * funcio_soroll(t)
     return [y + soroll, x - x**3]
+
+def edo_duffing(t, z):
+    """Paràmetres:
+        t: temps
+        z: posició al pla R^2
+    Retorna el camp vectorial del sistema d'EDOs x'=y+epsilon*random*f(t); y'=x-x^3.
+    """
+    x, y = z
+    return [y, x - x**3]
