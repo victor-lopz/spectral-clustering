f9055d254d8fa042e92ec8f9c67d986f03d4109e
Víctor López <217356204+victor-lopz@users.noreply.github.com>
2026-05-18 04:26:20 +0200

    rename parameters to match the latex document

--- src/ode.py
+++ src/ode.py
@@ -10,29 +10,29 @@ def edo_duffing_autonom(t, z):
     x, y = z
     return [y, x - x**3]
 
-
 def edo_duffing_no_autonom(t, z):
     """Paràmetres:
         t: temps
         z: posició al pla R^2
     Retorna el camp vectorial del sistema d'EDOs 
-    x' = -dPsi/dy; y' = dPsi/dx, on Psi és la funció de flux de Duffing no autònom.
+    x_dot = dPsi/dy; y_dot = -dPsi/dx, on Psi és la funció de corrent 
+    de Duffing no autònom de l'article d'Irina.
     """
     x, y = z
-    a = 0.5
-    eps = 0.1
-    omega = 3 * np.pi / 2
-    phi = np.pi / 4
-    cosinus = np.cos(omega * t + phi)
+    a = 0.5               # Coeficient de no linealitat
+    delta_0 = 0.1         # Amplitud base de la pertorbació
+    theta = 3 * np.pi / 2 # Freqüència angular
+    phi = np.pi / 4       # Fase inicial
+    cosinus = np.cos(theta * t + phi)
     
-    def epsilon(x):
-        return eps * (x + 4.5) / 2
+    def delta(x):
+        return delta_0 * (x + 4.5) / 2
     
-    def epsilon_dot(x):
-        return eps / 2
+    def delta_derivada(x):
+        return delta_0 / 2
   
     x_dot = y
-    first = - (epsilon_dot(x) * cosinus) * (x**2 / 2 - a * x**4 / 4)
-    second = (1 - epsilon(x) * cosinus) * (x - a * x**3)
+    first = - (delta_derivada(x) * cosinus) * (x**2 / 2 - a * x**4 / 4)
+    second = (1 - delta(x) * cosinus) * (x - a * x**3)
     y_dot = first + second
     return [x_dot, y_dot]
