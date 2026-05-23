4d061384c6152cc90c4f270edcfa9ea112154145
Víctor López <217356204+victor-lopz@users.noreply.github.com>
2026-05-23 19:31:27 +0200

    add: exported notebook for non-autonomous Duffing system, but only the parameters because it is the main difference between autonomous and non-autonomous notebooks

---
+++ notebooks/duffing_no_autonom.py
@@ -0,0 +1,12 @@
+params = ParametresGenerals(
+    dimensio = 2,
+    t_span = (0, 4 * np.pi),
+    t_steps = 300,
+    x_min = -2,
+    x_max = 2,
+    y_min = -1,
+    y_max = 1,
+    espai_entre_punts = 0.04,
+    max_clusters = 50,
+    num_radis = 50
+    )
