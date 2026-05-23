a59462b0b1a0d42b4ce091b73e7396c525a214de
Víctor López <217356204+victor-lopz@users.noreply.github.com>
2026-05-23 17:24:45 +0200

    remove: imprimeix_estadistics import because this function was deleted

--- src/__init__.py
+++ src/__init__.py
@@ -5,8 +5,7 @@ from .ode import (edo_duffing_autonom, edo_duffing_no_autonom)
 from .trajectories import (generar_condicions_inicials, generar_trajectories, 
                            calcula_matriu_pesos)
 
-from .spectral import (calcula_estadistics, 
-imprimeix_estadistics, sparcify, calcula_matriu_grau, calcula_vaps, sparcify_with_tol,
+from .spectral import (calcula_estadistics, sparcify, calcula_matriu_grau, calcula_vaps, sparcify_with_tol,
 calcula_num_clusters_i_max_eigengap, calcula_indicadors_vs_radis, troba_indexs_max_rel,
 troba_clusters, grafica_clusters_maxs_rel)
 
