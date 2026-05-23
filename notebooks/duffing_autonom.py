a227cebead206bf6539fdab95f3297adc3287a20
Víctor López <217356204+victor-lopz@users.noreply.github.com>
2026-05-23 19:18:02 +0200

    replace: export notebook to .py using this CLI command: jupytext --to py:bare duffing_autonom.ipynb

--- notebooks/duffing_autonom.py
+++ notebooks/duffing_autonom.py
@@ -1,16 +1,11 @@
-# %% [markdown]
-# ### Algorisme d'agrupament espectral aplicat al sistema de Duffing autònom
-
-# %%
 import sys
 sys.path.append("..")
 from src import *
 import numpy as np
 np.set_printoptions(precision=3, suppress=True)
-%load_ext autoreload
-%autoreload 2
+# %load_ext autoreload
+# %autoreload 2
 
-# %%
 params = ParametresGenerals(
     dimensio = 2,
     t_span = (0, 4 * np.pi),
@@ -24,23 +19,17 @@ params = ParametresGenerals(
     num_radis = 50
 )
 
-# %%
 condicions_inicials = generar_condicions_inicials(params)
 trajectories = generar_trajectories(edo_duffing_autonom, condicions_inicials, params)
 print("(Num trajectories, t_steps, dimensio) =", trajectories.shape)
 
-# %%
 matriu_pesos = calcula_matriu_pesos(trajectories)
 
-# %%
 pes_max = np.max(matriu_pesos)
 print(f"Pes màxim a la matriu de pesos: {pes_max:.3f}")
 constant_diagonal = pes_max * 1e7
 
-# %% [markdown]
-# ##### Opció A: esparsificar al 90%
-
-# %%
+# Opció A: esparsificar al 90%
 matriu_similaritat_W, radi_esparsificacio, percentatge_esparsificacio = \
       sparcify(matriu_pesos, percent=90)
 print(f"S'ha obtingut una esparsificació del {percentatge_esparsificacio*100:.0f}% "
@@ -58,21 +47,15 @@ labels = troba_clusters(num_clusters, veps)
 grafica_clusters(condicions_inicials, labels, num_clusters, radi_esparsificacio, 
                  percentatge_esparsificacio, params, subfolder="autonom")
 
-# %% [markdown]
-# ##### Opció B: mètode adaptatiu. Es trien els radis d'esparsificació que maximitzen l'eigengap màxim
-
-# %%
+# Opció B: mètode adaptatiu. 
+# Triar els radis d'esparsificació que maximitzen l'eigengap màxim
 result = calcula_indicadors_vs_radis(matriu_pesos, constant_diagonal, params)
 
-# %%
 indexs_max_rel = troba_indexs_max_rel(result.normalized_eigengaps)
 print("indexs_max_rel =", indexs_max_rel)
 print("radis[indexs_max_rel] =", result.radis[indexs_max_rel])
 grafica_clusters_maxs_rel(indexs_max_rel, result, condicions_inicials, 
                           params, subfolder="autonom_maxs_rels")
 
-# %%
 grafica_eigengaps_vs_radi(result, params, indexs_max_rel=indexs_max_rel, 
                           subfolder="autonom_maxs_rels")
-
-
