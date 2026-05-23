570c3a4a2feebe22d34f223afc3e6cce39199d6a
Víctor López <217356204+victor-lopz@users.noreply.github.com>
2026-05-23 16:07:58 +0200

    Revert "add: coherence metric module and refactor imports."
    This reverts commit b8aa6af4a5cc8d81114812774d0bf6d58b301211.

--- src/__init__.py
+++ src/__init__.py
@@ -1,38 +1,18 @@
 # src/__init__.py
 
-from .datatypes import (ParametresGenerals, SpectralAnalysisResult)
-
 from .ode import (edo_duffing_autonom, edo_duffing_no_autonom)
 
-from .trajectories import (
-    generar_condicions_inicials,
-    generar_trajectories,
-    calcula_matriu_pesos,
-)
+from .trajectories import (generar_condicions_inicials, generar_trajectories, 
+                           calcula_matriu_pesos)
 
-from .spectral import (
-    sparcify_with_tol,
-    sparcify,
-    calcula_vaps,
-    calcula_num_clusters_i_max_eigengap,
-    troba_clusters,
-    calcula_indicadors_vs_radis,
-    troba_indexs_max_rel,
-    grafica_clusters_maxs_rel,
-)
+from .spectral import (calcula_estadistics, 
+imprimeix_estadistics, sparcify, calcula_matriu_grau, calcula_vaps, sparcify_with_tol,
+calcula_num_clusters_i_max_eigengap, calcula_indicadors_vs_radis, troba_indexs_max_rel,
+troba_clusters, grafica_clusters_maxs_rel)
 
-from .plotting import (
-    grafica_trajectories,
-    grafica_punts,
-    grafica_eigengaps_vs_radi,
-    grafica_clusters,
-    grafica_eigenvalues_vs_index,
-    grafica_eigengaps_vs_index,
-)
+from .plotting import (grafica_trajectories, grafica_punts, grafica_eigengaps_vs_radi,
+grafica_clusters, grafica_eigenvalues_vs_index, grafica_eigengaps_vs_index)
 
-from .metrica_coherent import (grafica_coherencia)
+from .utils import (troba_radi_optim, aplica_spectral_clustering_optim)
 
-from .utils import (
-    troba_radi_optim,
-    aplica_spectral_clustering_optim,
-)
+from .datatypes import (ParametresGenerals, SpectralAnalysisResult)
